e5b8eb90876be6c5b8f24212b00be8c9119462ac
unknown <Administrator@GIH-D-32060Z.yajuenet.internal>
2026-03-20 11:10:19 +0800

    feat: VOL3单独部署至 /dahlia-v3, 所有页面互不影响

--- vol3/next.config.js
+++ vol3/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/SOME_DEMO',
+  basePath: '/SOME_DEMO/dahlia-v3',
   images: { unoptimized: true },
   // 关闭 React 严格模式（R3F 在严格模式下会 double mount 导致 WebGL 资源泄漏）
   reactStrictMode: false,
